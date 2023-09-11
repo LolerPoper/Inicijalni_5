a0a711b4d524404009f6a2c6d116dc46c3f48112
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2023-09-11 11:01:52 +0200

    Add files via upload

---
+++ Inicijalni 5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalni_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+
+            Console.WriteLine("Upišite link web stranice: ");
+            a = Console.ReadLine();
+
+            if(a.StartsWith("www.") && a.EndsWith(".hr"))
+            {
+                Console.WriteLine("Ovo je ispravni link u hrvatskom formatu.");
+            }
+            else
+            {
+                Console.WriteLine("Ovo nije ispravni link u hrvatskom formatu.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
